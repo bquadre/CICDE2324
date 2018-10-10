a99385c04a59b3cecf84f4eba3a9ec92204285d8
Babatunde <quadremustapha@gmail.com>
2018-10-10 15:24:27 +0100

    Add project files.

---
+++ CICD2324/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICD2324.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
